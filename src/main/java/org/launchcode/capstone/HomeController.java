4ad7066db18478c45b26c408e68f2bbf51dd1d3d
Milani <PMilan000@cable.comcast.com>
2020-06-03 21:56:25 -0400

    Front page setup

--- src/main/java/org/launchcode/capstone/HomeController.java
+++ src/main/java/org/launchcode/capstone/HomeController.java
@@ -1,10 +1,16 @@
 package org.launchcode.capstone;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-
+    @RequestMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "PDF Manager" );
+        return "index";
+    }
 
 }
